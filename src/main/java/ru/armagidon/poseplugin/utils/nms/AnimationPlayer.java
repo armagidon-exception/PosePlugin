26aaf1006df3ba1004ff108023d4cde9b376cf8a
armagidon-exception <yt.armagidon.mc@gmail.com>
2020-06-26 10:37:11 +1000

    Performing to add 1.16 support

--- src/main/java/ru/armagidon/poseplugin/utils/nms/AnimationPlayer.java
+++ src/main/java/ru/armagidon/poseplugin/utils/nms/AnimationPlayer.java
@@ -1,46 +1,29 @@
 package ru.armagidon.poseplugin.utils.nms;
 
-import net.minecraft.server.v1_15_R1.*;
-import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Pose;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
+import static ru.armagidon.poseplugin.utils.nms.NMSUtils.getNmsClass;
 import static ru.armagidon.poseplugin.utils.nms.NMSUtils.sendPacket;
 
 public interface AnimationPlayer
 {
     static void play(Player target, Player receiver, Pose pose) {
-        EntityPlayer vanillaplayer = ((CraftPlayer)target).getHandle();
-        EntityHuman human = new EntityHuman(vanillaplayer.getWorld(), vanillaplayer.getProfile()) {
-
-            public boolean isSpectator() {
-                return false;
-            }
-
-
-            public boolean isCreative() {
-                return false;
-            }
-        };
-
-        human.e(vanillaplayer.getId());
-        DataWatcher parentwatcher = vanillaplayer.getDataWatcher();
-        byte overlays = parentwatcher.get(DataWatcherRegistry.a.a(16));
-        byte arrows = parentwatcher.get(DataWatcherRegistry.a.a(0));
-        DataWatcher watcher =human.getDataWatcher();
-        watcher.set(DataWatcherRegistry.a.a(16), overlays);
-        watcher.set(DataWatcherRegistry.a.a(0),arrows);
         try{
-            Field watcherField = Entity.class.getDeclaredField("datawatcher");
-            watcherField.setAccessible(true);
-            watcherField.set(human, watcher);
-        } catch (Exception e){
+            Object vanilla = NMSUtils.asNMSCopy(target);
+            int id = NMSUtils.getPlayerID(target);
+
+            Object watcher = NMSUtils.getNmsClass("Entity").getDeclaredMethod("getDataWatcher").invoke(vanilla);
+
+            NMSUtils.setPlayerPose(target, pose);
+
+            Object metadata = NMSUtils.createPacketInstance("PacketPlayOutEntityMetadata",new Class[]{int.class, getNmsClass("DataWatcher"), boolean.class}, id,watcher, false);
+
+            sendPacket(receiver, metadata);
+        } catch (IllegalAccessException | InvocationTargetException | ClassNotFoundException | NoSuchMethodException | InstantiationException e) {
             e.printStackTrace();
         }
-        human.getDataWatcher().set(DataWatcherRegistry.s.a(6),EntityPose.SWIMMING);
-        PacketPlayOutEntityMetadata metadata = new PacketPlayOutEntityMetadata(vanillaplayer.getId(),human.getDataWatcher(), false);
-        sendPacket(receiver, metadata);
     }
 }
--- src/main/java/ru/armagidon/poseplugin/utils/nms/NMSUtils.java
+++ src/main/java/ru/armagidon/poseplugin/utils/nms/NMSUtils.java
@@ -1,41 +1,76 @@
 package ru.armagidon.poseplugin.utils.nms;
 
-import net.minecraft.server.v1_15_R1.Packet;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import ru.armagidon.poseplugin.PosePlugin;
+import org.bukkit.entity.Pose;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class NMSUtils
 {
 
-    public static void sendPacket(Player receiver, Packet<?> packet) {
+    public static void sendPacket(Player receiver, Object packet){
         try {
-            Object nmsPlayer = receiver.getClass().getMethod("getHandle").invoke(receiver);
+            Object nmsPlayer = asNMSCopy(receiver);
             Object plrConnection = nmsPlayer.getClass().getField("playerConnection").get(nmsPlayer);
             plrConnection.getClass().getMethod("sendPacket", getNmsClass("Packet")).invoke(plrConnection, packet);
-        } catch (Exception e){
-            PosePlugin.getInstance().getLogger().severe(e.toString());
-        }
-    }
-
-    private static Class<?> getNmsClass(String nmsClassName) throws Exception {
-        return Class.forName("net.minecraft.server." + Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3] + "." + nmsClassName);
-    }
-
-    static void setField(String name, Object source, Object value, Class<?> clazz){
-        try {
-            Field dW = clazz.getDeclaredField(name);
-            dW.setAccessible(true);
-            dW.set(source, value);
-        } catch (Exception e){
+        } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException | NoSuchFieldException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public static void setInvisible(Player player, boolean invisible){
-        FakePlayer.asNMSCopy(player).setInvisible(invisible);
+    public static Class<?> getNmsClass(String nmsClassName) throws ClassNotFoundException {
+        return Class.forName("net.minecraft.server." + Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3] + "." + nmsClassName);
+    }
+
+    public static void setInvisible(Player player, boolean invisible) {
+        try {
+            Object handle = NMSUtils.asNMSCopy(player);
+            Method setInvisible = handle.getClass().getDeclaredMethod("setInvisible",boolean.class);
+            setInvisible.setAccessible(true);
+            setInvisible.invoke(handle, invisible);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setPlayerPose(Player target, Pose pose) {
+        try {
+            Object handle = asNMSCopy(target);
+            Class<?> entityClass = getNmsClass("Entity");
+            Method setPose = entityClass.getDeclaredMethod("setPose", getNmsClass("EntityPose"));
+            setPose.setAccessible(true);
+
+            Enum<?> value = getEnumValues(getEnum("EntityPose"))[pose.ordinal()];
+            setPose.invoke(handle, value);
+        } catch (ClassNotFoundException | InvocationTargetException | IllegalAccessException | NoSuchMethodException e){
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <E extends Enum> E[] getEnumValues(Class<E> enumClass) {
+        return enumClass.getEnumConstants();
+    }
+
+    public static Object asNMSCopy(Player player) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method m = player.getClass().getDeclaredMethod("getHandle");
+        m.setAccessible(true);
+        return m.invoke(player);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static Class<Enum<?>> getEnum(String name) throws ClassNotFoundException {
+        return (Class<Enum<?>>) getNmsClass(name);
+    }
+
+    public static int getPlayerID(Player player) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
+        return (int) getNmsClass("Entity").getDeclaredMethod("getId").invoke(asNMSCopy(player));
+    }
+
+    public static Object createPacketInstance(String name, Class[] types, Object... params) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        return getNmsClass(name).getConstructor(types).newInstance(params);
     }
 
 }
