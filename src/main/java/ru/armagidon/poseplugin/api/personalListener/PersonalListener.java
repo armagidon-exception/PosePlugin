cb949f7636522cb3b31007af38148e087bebffd3
armagidon-exception <yt.armagidon.mc@gmail.com>
2020-06-27 22:47:47 +1000

    Repository clear

--- src/main/java/ru/armagidon/poseplugin/api/personalListener/PersonalListener.java
+++
@@ -1,4 +0,0 @@
-package ru.armagidon.poseplugin.api.personalListener;
-
-public interface PersonalListener {
-}
--- src/main/java/ru/armagidon/poseplugin/api/poses/personalListener/PersonalListener.java
+++
@@ -1,4 +0,0 @@
-package ru.armagidon.poseplugin.api.poses.personalListener;
-
-public interface PersonalListener {
-}
