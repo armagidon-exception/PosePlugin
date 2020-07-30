b8d0aecb788e7cee59b660f7dce975429a30552b
armagidon-exception <yt.armagidon.mc@gmail.com>
2020-07-30 17:08:52 +0300

    Added ItemUtil class

--- api/src/main/java/ru/armagidon/poseplugin/api/utils/nms/NMSFactory.java
+++ api/src/main/java/ru/armagidon/poseplugin/api/utils/nms/NMSFactory.java
@@ -1,39 +1,41 @@
 package ru.armagidon.poseplugin.api.utils.nms;
 
+import lombok.SneakyThrows;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Pose;
+import org.bukkit.inventory.ItemStack;
+import ru.armagidon.poseplugin.api.utils.items.ItemUtil;
 import ru.armagidon.poseplugin.api.utils.nms.npc.FakePlayer;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 public class NMSFactory
 {
     private final Constructor<?> fakeplayer;
     private final Constructor<?> playerhider;
+    private final Constructor<?> itemUtil;
 
     public NMSFactory() throws ClassNotFoundException, NoSuchMethodException {
         this.fakeplayer = Class.forName("ru.armagidon.poseplugin.api.utils.nms.npc.FakePlayer_"+ReflectionTools.nmsVersion()).getDeclaredConstructor(Player.class, Pose.class);
         fakeplayer.setAccessible(true);
         this.playerhider = Class.forName("ru.armagidon.poseplugin.api.utils.nms.PlayerHider_"+ReflectionTools.nmsVersion()).getDeclaredConstructor();
         playerhider.setAccessible(true);
+        itemUtil = Class.forName("ru.armagidon.poseplugin.api.utils.nms.item.ItemUtil_"+ReflectionTools.nmsVersion()).getDeclaredConstructor(ItemStack.class);
+        itemUtil.setAccessible(true);
     }
 
+    @SneakyThrows
     public FakePlayer createFakePlayer(Player player, Pose pose){
-        try {
-            return (FakePlayer) fakeplayer.newInstance(player,pose);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-            return null;
-        }
+        return (FakePlayer) fakeplayer.newInstance(player,pose);
     }
 
+    @SneakyThrows
     public PlayerHider createPlayerHider(){
-        try {
-            return (PlayerHider) playerhider.newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-            return null;
-        }
+        return (PlayerHider) playerhider.newInstance();
+    }
+
+    @SneakyThrows
+    public ItemUtil createItemUtil(ItemStack item){
+        return (ItemUtil) itemUtil.newInstance(item);
     }
 }
