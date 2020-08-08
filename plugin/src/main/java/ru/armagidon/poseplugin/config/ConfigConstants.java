ffe41c1b5f4031e460c1e9471b961b66284025c3
armagidon-exception <yt.armagidon.mc@gmail.com>
2020-08-08 21:00:55 +0300

    Added self-repairable config

---
+++ plugin/src/main/java/ru/armagidon/poseplugin/config/ConfigConstants.java
@@ -0,0 +1,30 @@
+package ru.armagidon.poseplugin.config;
+
+import ru.armagidon.poseplugin.PosePlugin;
+
+public class ConfigConstants
+{
+    public static boolean checkForUpdates(){
+        return PosePlugin.getInstance().getConfig().getBoolean("check-for-updates");
+    }
+
+    public static boolean experimentalMode(){
+        return PosePlugin.getInstance().getConfig().getBoolean("x-mode");
+    }
+
+    public static boolean isWaveEnabled(){
+        return experimentalMode()&&PosePlugin.getInstance().getConfig().getBoolean("wave.enabled");
+    }
+
+    public static boolean isPointEnabled(){
+        return experimentalMode()&&PosePlugin.getInstance().getConfig().getBoolean("point.enabled");
+    }
+
+    public static boolean isSwimEnabled(){
+        return PosePlugin.getInstance().getConfig().getBoolean("swim.enabled");
+    }
+
+    public static String locale(){
+        return PosePlugin.getInstance().getConfig().getString("locale","en");
+    }
+}
---
+++ plugin/src/main/java/ru/armagidon/poseplugin/config/ConfigManager.java
@@ -0,0 +1,90 @@
+package ru.armagidon.poseplugin.config;
+
+import lombok.SneakyThrows;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import ru.armagidon.poseplugin.PosePlugin;
+
+import java.io.File;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ConfigManager 
+{
+
+    private final File configFile;
+    private FileConfiguration config;
+
+    @SneakyThrows
+    public ConfigManager() {
+        configFile = new File(PosePlugin.getInstance().getDataFolder(), "config.yml");
+        if(!configFile.exists()) configFile.createNewFile();
+        config = YamlConfiguration.loadConfiguration(configFile);
+        fixConfig();
+    }
+
+    @SneakyThrows
+    private void fixConfig() {
+        YamlConfiguration defaults = new YamlConfiguration();
+        {
+            defaults.set("locale", "en");
+            defaults.set("check-for-updates", true);
+            defaults.set("x-mode", false);
+            {
+                ConfigurationSection swim = defaults.createSection("swim");
+                swim.set("static", false);
+                swim.set("stand-up-when-damaged", true);
+                swim.set("enabled", true);
+            }
+            {
+                ConfigurationSection sit = defaults.createSection("sit");
+                sit.set("stand-up-when-damage", true);
+            }
+            {
+                ConfigurationSection lay = defaults.createSection("lay");
+                lay.set("stand-up-when-damage", true);
+                lay.set("view-distance", 20);
+                lay.set("head-rotation", true);
+                lay.set("swing-animation", true);
+                lay.set("update-equipment", true);
+                lay.set("update-overlays", true);
+                lay.set("prevent-use-when-invisible", false);
+            }
+            if (config.getBoolean("x-mode")) {
+                {
+                    ConfigurationSection wave = defaults.createSection("wave");
+                    wave.set("enabled", true);
+                }
+                {
+                    ConfigurationSection point = defaults.createSection("point");
+                    point.set("enabled", true);
+                }
+            }
+        }
+        AtomicInteger integer = new AtomicInteger(0);
+        Set<String> defaultKeys = defaults.getKeys(true);
+        Set<String> presentedKeys = config.getKeys(true);
+        defaultKeys.forEach(key->{
+            if(!presentedKeys.contains(key)){
+                integer.incrementAndGet();
+                Object defaultValue = defaults.get(key);
+                config.addDefault(key, defaultValue);
+                config.set(key, defaultValue);
+            }
+        });
+        if(integer.get()!=0) {
+            config.options().copyDefaults(true);
+            config.save(configFile);
+        }
+    }
+
+    public FileConfiguration getConfig() {
+        return config;
+    }
+
+    public void reload() {
+        this.config = YamlConfiguration.loadConfiguration(this.configFile);
+        fixConfig();
+    }
+}
