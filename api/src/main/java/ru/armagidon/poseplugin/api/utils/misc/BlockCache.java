fb995dc967bccb7934028b0f6a459e827773bd2f
armagidon-exception <yt.armagidon.mc@gmail.com>
2020-08-08 21:05:53 +0300

    Code clean up

--- api/src/main/java/ru/armagidon/poseplugin/api/utils/misc/BlockCache.java
+++ api/src/main/java/ru/armagidon/poseplugin/api/utils/misc/BlockCache.java
@@ -1,36 +1,26 @@
 package ru.armagidon.poseplugin.api.utils.misc;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 
 public class BlockCache {
 
-        private final Material material;
-        private final BlockData data;
-        private final Location location;
+    private @Getter @Setter BlockData data;
+    private @Getter @Setter Location location;
 
-        public BlockCache(Material material, BlockData data, Location location) {
-            this.material = material;
-            this.data = data;
-            this.location = location;
-        }
+    public BlockCache(BlockData data, Location location) {
+        this.data = data;
+        this.location = location;
+    }
 
-        public Location getLocation() {
-            return location;
-        }
-
-        public BlockData getData() {
-            return data;
-        }
-
-        public void restore(){
-            getLocation().getBlock().setType(material);
-            getLocation().getBlock().setBlockData(data);
-        }
+    public void restore(){
+        getLocation().getBlock().setBlockData(data);
+    }
 
     public void restore(Player receiver) {
-            receiver.sendBlockChange(getLocation(), getData());
+        receiver.sendBlockChange(getLocation(), getData());
     }
 }
