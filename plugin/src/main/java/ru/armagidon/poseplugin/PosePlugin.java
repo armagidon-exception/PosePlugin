c15eceac76c72999372c803a6a67daa835a2f23c
armagidon-exception <yt.armagidon.mc@gmail.com>
2020-08-08 21:00:32 +0300

    Changed main class

--- plugin/src/main/java/ru/armagidon/poseplugin/PosePlugin.java
+++ plugin/src/main/java/ru/armagidon/poseplugin/PosePlugin.java
@@ -1,25 +1,21 @@
 package ru.armagidon.poseplugin;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.PluginCommand;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 import ru.armagidon.poseplugin.api.PosePluginAPI;
-import ru.armagidon.poseplugin.api.utils.misc.Debugger;
+import ru.armagidon.poseplugin.command.PluginCommands;
+import ru.armagidon.poseplugin.config.ConfigConstants;
+import ru.armagidon.poseplugin.config.ConfigManager;
 import ru.armagidon.poseplugin.plugin.PluginEventListener;
 import ru.armagidon.poseplugin.plugin.UpdateChecker;
 import ru.armagidon.poseplugin.plugin.messaging.Messages;
 
-import java.util.ArrayList;
-
 public final class PosePlugin extends JavaPlugin implements Listener
 {
     private static PosePlugin instance;
+    private final ConfigManager configManager;
 
     public static PosePlugin getInstance() {
         return instance;
@@ -27,18 +23,15 @@ public final class PosePlugin extends JavaPlugin implements Listener
 
     public static UpdateChecker checker;
     private Messages messages;
-    private final FileConfiguration config;
-
-    private final Debugger debugger;
 
     private PluginEventListener listener;
 
+
     public PosePlugin() {
         instance = this;
-        this.debugger = new Debugger();
-        this.config = getConfig();
+        configManager = new ConfigManager();
         try {
-            this.messages = new Messages(config.getString("locale", "en"));
+            this.messages = new Messages(ConfigConstants.locale());
         } catch (IllegalArgumentException e){
             setEnabled(false);
             getLogger().severe(e.getMessage());
@@ -52,23 +45,8 @@ public final class PosePlugin extends JavaPlugin implements Listener
         getServer().getPluginManager().registerEvents(listener,this);
         //Init commands
         initCommands();
-        //Save config
-        saveDefaultConfig();
         //Check for updates
         checkForUpdates();
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
-    {
-        if(command.getName().equalsIgnoreCase("ppreload")){
-            Bukkit.getPluginManager().disablePlugin(this);
-            reloadConfig();
-            Bukkit.getPluginManager().enablePlugin(this);
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&8&l[&b&l&nPosePlugin&8&l]&a Plugin reloaded!"));
-            return true;
-        }
-        return true;
     }
 
     @Override
@@ -77,32 +55,8 @@ public final class PosePlugin extends JavaPlugin implements Listener
     }
 
     private void initCommands(){
-        TabCompleter c = (commandSender, command, s, strings) -> new ArrayList<>();
-        PluginCommand sit =getCommand("sit");
-        PluginCommand lay =getCommand("lay");
-        PluginCommand swim =getCommand("swim");
-        PluginCommand ppreload = getCommand("ppreload");
-        if(ppreload!=null){
-            ppreload.setExecutor(this);
-            ppreload.setTabCompleter(c);
-        }
         PluginCommands pcs = new PluginCommands();
-        if(sit!=null) {
-            sit.setExecutor(pcs);
-            sit.setTabCompleter(c);
-        }
-        if(lay!=null) {
-            lay.setExecutor(pcs);
-            lay.setTabCompleter(c);
-        }
-        if(swim!=null) {
-            swim.setExecutor(pcs);
-            swim.setTabCompleter(c);
-        }
-    }
-
-    public Debugger getDebugger() {
-        return debugger;
+        pcs.initCommands();
     }
 
     public Messages message(){
@@ -110,7 +64,7 @@ public final class PosePlugin extends JavaPlugin implements Listener
     }
 
     private void checkForUpdates(){
-        if(config.getBoolean("check-for-updates")){
+        if(ConfigConstants.checkForUpdates()){
             checker = new UpdateChecker();
             checker.runTaskAsynchronously(this);
         }
@@ -118,5 +72,16 @@ public final class PosePlugin extends JavaPlugin implements Listener
 
     public PluginEventListener getListener() {
         return listener;
+    }
+
+    @Override
+    public void reloadConfig() {
+        configManager.reload();
+    }
+
+    @NotNull
+    @Override
+    public FileConfiguration getConfig() {
+        return configManager.getConfig();
     }
 }
